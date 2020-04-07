ad78449a29e3a65f24d0f4ebd8143adf0403585e
Rusty Eddy <rusty@rustyeddy.com>
2020-04-07 12:35:43 -0700

    added things like org mode

--- init.el
+++ init.el
@@ -10,17 +10,12 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
-
 ;; ************************* Package ************************************
 (when (>= emacs-major-version 24)
   (require 'package)
-  (add-to-list
-   'package-archives
-					; many packages won't show if using stable
-   '("melpa" . "http://stable.melpa.org/packages/") 
-   ;; '("melpa" . "http://melpa.milkbox.net/packages/")
-   t))
+  (add-to-list 'package-archives
+	       '("melpa" . "http://stable.melpa.org/packages/"))
+  (package-initialize))
 
 ;; ======================================================================
 ;; PATHS Add our local elisp directory to the path
@@ -29,21 +24,35 @@
 (add-to-list 'load-path "~/.emacs.d/elisp")
 (add-to-list 'load-path "~/.emacs.d/vendor")
 
+;; Org Mode
+;; ======================================================================
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
+
+
 ;; --------------------- Mac Meta/Option Keys --------------------------
 ;; I prefer cmd key for meta. This is just set in emacs
 ;; ONLY on a MAC
 ;;
-(setq mac-option-key-is-meta nil
-      mac-command-key-is-meta t
-      mac-command-modifier 'meta
-      mac-option-modifier 'none)
-
+;; (setq mac-option-key-is-meta nil
+;;       mac-command-key-is-meta t
+;;       mac-command-modifier 'meta
+;;       mac-option-modifier 'none)
 ;;; -------------------- Decent Looking Theme ----------------------------
 ;;; Added railscasts mode
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
 ;;; load rails casts 
 (load-theme 'railscasts-reloaded t)
+
+;;; ---------------------- ORG MODE -----------------------------------
+(setq org-log-done 'time)
+(setq org-directory "~/org")
+(setq org-default-notes-file (concat org-directory "/log/notes.org"))
+(define-key global-map "\C-cc" 'org-capture)
+
 
 ;;; -----------------------     Lines     --------------------------------
 ;;; Turn on linum mode - give linum a little extra gutter space
@@ -62,6 +71,7 @@
 ;;; Setup Emacs Server
 (server-start)
 
+;; set my email address
 (setq user-mail-address "rusty@rustyeddy.com")
 
 ;; ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
